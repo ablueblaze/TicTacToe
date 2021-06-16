ad8734e7264842a7d303a0fa877f736c175a4eb3
Blaze <blazeascott@gmail.com>
2021-06-15 22:07:35 -0500

    Played with a test object to try and figure out the structure of a for loop to append all the necisary elements. And It WORKED!!!

--- index.js
+++ index.js
@@ -13,10 +13,36 @@ const gameBoard = (() => {
     cell.className = "cell";
     row.className = "row";
 
+    const board = document.getElementById("board")
+
+    for (let i in gameBoardObject){
+      board.appendChild(row);
+    }
+
   }
 
-  return {gameBoardObject, }
+  return {gameBoardObject, generateBoard}
 })();
+
+// gameBoard.generateBoard();
+
+const test = {
+  first: [1,2,3],
+  second: [4,5,6],
+  third: [7,8,9]
+}
+
+for (const key of Object.keys(test)){
+  const board = document.getElementById('board');
+  const div = document.createElement('div');
+  for (let values of test[key]){
+    const span = document.createElement('span');
+    let text = document.createTextNode(values);
+    span.appendChild(text);
+    div.appendChild(span);
+  }
+  board.appendChild(div);
+}
 
 const winCondition = function(gameState){
   // All three of a single row are one player
