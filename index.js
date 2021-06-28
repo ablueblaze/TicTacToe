ab535c162f57a7fa3d32e7156a90919e5cf05a18
Blaze <blazeascott@gmail.com>
2021-06-28 05:16:44 -0500

    Not super pretty, but it works. Deffinatly not in the clean code field though. So I need to work on that.

--- index.js
+++ index.js
@@ -47,45 +47,39 @@ const gameController = (() => {
     // All three rows with the same index as the same player
     // Index 0,1,2 in row1, row2, row3
     // Index 2,1,0 in row1, row2, row3
-
-
-    let currentBoardList = [];
-
-    let colum1 = [];
-    let colum2 = [];
-    let colum3 = [];
-    let cross1 = [];
-    let cross2 = [];
-    let count = 0;
-
-    for (let key of Object.keys(test)){
-      currentBoardList.push(test[key])
-      colum1.push(test[key][0])
-      colum2.push(test[key][1])
-      colum3.push(test[key][2])
-      cross1.push(test[key][0 + count])
-      cross2.push(test[key][2 - count])
-      count++
-
-      if (colum1.length == 2){
-        currentBoardList.push(colum1, colum2, colum3, cross1, cross2)
+    const activeBoardCheck = () => {
+      let currentBoardList = [];
+      let colum1 = [];
+      let colum2 = [];
+      let colum3 = [];
+      let cross1 = [];
+      let cross2 = [];
+      let count = 0;
+  
+      for (let key of Object.keys(boardTools.gameBoard)){
+        currentBoardList.push(boardTools.gameBoard[key])
+        colum1.push(boardTools.gameBoard[key][0])
+        colum2.push(boardTools.gameBoard[key][1])
+        colum3.push(boardTools.gameBoard[key][2])
+        cross1.push(boardTools.gameBoard[key][0 + count])
+        cross2.push(boardTools.gameBoard[key][2 - count])
+        count++
+        if (count == 3){
+          currentBoardList.push(colum1, colum2, colum3, cross1, cross2)
+          for (let i = 0; i < currentBoardList.length; i++)
+            if (
+              currentBoardList[i][0] == player &&
+              currentBoardList[i][1] == player &&
+              currentBoardList[i][2] == player ){
+              console.log(`${player} Wins!`)
+            } 
+          }
+        }
+  
       }
+      activeBoardCheck();
     }
-
-    for (let key of Object.keys(boardTools.gameBoard)){
-      // All three of a single row are one player
-
-      let play = boardTools.gameBoard[key];
-      console.log(`play: ${play} player: ${player}`)
-      if (
-        play[0] == player &&
-        play[1] == player &&
-        play[2] == player ){
-        console.log(`${player} Wins! (Horizontal)`)
-      } 
-        index++
-    }
-  }
+  
 
   const boardUpdate = function(key, index){
     currentPlay = boardTools.gameBoard[key][index]
@@ -93,7 +87,6 @@ const gameController = (() => {
       boardTools.gameBoard[key][index] = player;
       cell = player;
     }
-
     if (currentPlay == 0){
       playCount++
       if (playCount % 2 != 0){
