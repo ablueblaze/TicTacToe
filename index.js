f7e4835ef3131cce4875287580e5fdcce5520f66
Blaze <blazeascott@gmail.com>
2021-07-02 00:20:35 -0500

    Change the board generator to use div's instead of span. I just learned what the difference is!!!

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ const boardTools = (() => {
     row3: [0,0,0]
   };
 
-  
-
   const generateBoard = function(){
     const board = document.getElementById("board");
     let key = 1;
     let index = 0;
     for (let i = 0; i < 3; i++){
+      let row = document.createElement("div")
+      row.className = "row";
+      board.appendChild(row)
       for (let n = 0; n < 3; n++){
-        const cell = document.createElement("span");
+        let cell = document.createElement("div");
         cell.dataset.key = `row${key}`;
         cell.dataset.index = index;
         cell.className = "cell";
-        board.appendChild(cell);
+        row.appendChild(cell);
         index++
       }
       key++;
@@ -39,47 +40,45 @@ const player = function(name, score){
 let player1 = player("X", 0);
 let player2 = player("O", 0);
 
-
 const gameController = (() => {
   let playCount = 0;
+  let gameOver = false;
   
-  const winner = function(player){
-    // All three rows with the same index as the same player
-    // Index 0,1,2 in row1, row2, row3
-    // Index 2,1,0 in row1, row2, row3
-    const activeBoardCheck = () => {
-      let currentBoardList = [];
-      let colum1 = [];
-      let colum2 = [];
-      let colum3 = [];
-      let cross1 = [];
-      let cross2 = [];
-      let count = 0;
-  
-      for (let key of Object.keys(boardTools.gameBoard)){
-        currentBoardList.push(boardTools.gameBoard[key])
-        colum1.push(boardTools.gameBoard[key][0])
-        colum2.push(boardTools.gameBoard[key][1])
-        colum3.push(boardTools.gameBoard[key][2])
-        cross1.push(boardTools.gameBoard[key][0 + count])
-        cross2.push(boardTools.gameBoard[key][2 - count])
-        count++
-        if (count == 3){
-          currentBoardList.push(colum1, colum2, colum3, cross1, cross2)
-          for (let i = 0; i < currentBoardList.length; i++)
-            if (
-              currentBoardList[i][0] == player &&
-              currentBoardList[i][1] == player &&
-              currentBoardList[i][2] == player ){
-              console.log(`${player} Wins!`)
-            } 
-          }
-        }
-  
+  const activeBoard = () => {
+    let currentBoard = [];
+    let colum1 = [];
+    let colum2 = [];
+    let colum3 = [];
+    let cross1 = [];
+    let cross2 = [];
+    let count = 0;
+
+    for (let key of Object.keys(boardTools.gameBoard)){
+      currentBoard.push(boardTools.gameBoard[key])
+      colum1.push(boardTools.gameBoard[key][0])
+      colum2.push(boardTools.gameBoard[key][1])
+      colum3.push(boardTools.gameBoard[key][2])
+      cross1.push(boardTools.gameBoard[key][0 + count])
+      cross2.push(boardTools.gameBoard[key][2 - count])
+      count++
+      if (count == 3){
+        currentBoard.push(colum1, colum2, colum3, cross1, cross2)
+        return currentBoard;
       }
-      activeBoardCheck();
     }
+  }
   
+  const findWinner = function(player){
+    currentBoard = activeBoard();
+    for (let i = 0; i < currentBoard.length; i++)
+      if (
+        currentBoard[i][0] == player &&
+        currentBoard[i][1] == player &&
+        currentBoard[i][2] == player ){
+        console.log(`${player} Wins!`);
+        gameOver = true;
+      } 
+    }
 
   const boardUpdate = function(key, index){
     currentPlay = boardTools.gameBoard[key][index]
@@ -91,28 +90,29 @@ const gameController = (() => {
       playCount++
       if (playCount % 2 != 0){
         play(player1.name);
-        winner(player1.name);
+        findWinner(player1.name);
         return player1.name;
       } else {
         play(player2.name);
-        winner(player2.name);
+        findWinner(player2.name);
         return player2.name;
       }
     } else {return currentPlay}
   }
-
+  
   const playSelector = document.querySelector("body");
   playSelector.addEventListener("click", (e) => {
       let target = e.target;
-      if(target.className == "cell"){
-        let key = target.dataset.key;
-        let index = parseFloat(target.dataset.index);
-        target.textContent = boardUpdate(key, index);
-        winner();
+      if (gameOver == false){
+        if(target.className == "cell"){
+          let key = target.dataset.key;
+          let index = parseFloat(target.dataset.index);
+          target.textContent = boardUpdate(key, index);
+        }
       }
-    }
-  )
-  return {winner}
+    })
+
+  return {findWinner}
 })();
 
 boardTools.generateBoard();
